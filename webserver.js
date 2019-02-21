7be608a0b89e4659342a0f40f4dd500e6ef45022
fengfancky <fengfancky@gmail.com>
2019-02-21 16:19:04 +0800

    web matrix

---
+++ webserver.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var ContentType = require('./server/content_type')
+var contentType = new ContentType();
+
+http.createServer(function(request,response){
+
+    var pathname = url.parse(request.url).pathname;
+
+    var type = contentType.getType(pathname);
+
+    fs.readFile(pathname.substr(1), function (err, data) {
+       if (err) {
+          console.log(err);
+          response.writeHead(404, {'Content-Type': 'text/html;charset=UTF-8'});
+       }else{             
+          // HTTP 状态码: 200 : OK
+          // Content Type: text/plain
+          response.writeHead(200, {'Content-Type': type});    
+          
+          if(pathname.endsWith('.png')||pathname.endsWith('.jpg')){
+             response.write(data,'binary');   
+             
+          }else{
+             response.write(data);  
+          }
+            
+       }
+       //  发送响应数据
+       response.end();
+    });  
+
+}).listen(8080);
+
+console.log('start server');
